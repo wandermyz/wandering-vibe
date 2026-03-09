53326a0a05153446b37c222c62318d7b096bd25a
wandermyz.yuki <lywander@gmail.com>
2026-03-09 12:25:13 -0700

    Revert "Suspend app on close to return to the sharing app"
    This reverts commit 643e5c5c4a87f508a02fe09beadab11adfb5bd52.

--- wandering-md/wandering-md/ContentView.swift
+++ wandering-md/wandering-md/ContentView.swift
@@ -1,5 +1,4 @@
 import SwiftUI
-import UIKit
 
 struct ContentView: View {
     @Binding var markdownContent: String?
@@ -16,14 +15,6 @@ struct ContentView: View {
                             Button {
                                 markdownContent = nil
                                 fileName = nil
-                                // Suspend the app to return to the app that shared the file.
-                                // iOS has no public API to navigate back to the previous app,
-                                // but suspending achieves the same effect.
-                                UIControl().sendAction(
-                                    #selector(URLSessionTask.suspend),
-                                    to: UIApplication.shared,
-                                    for: nil
-                                )
                             } label: {
                                 Image(systemName: "xmark")
                             }
